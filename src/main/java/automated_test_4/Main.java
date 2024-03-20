08308dd69c38fce008d88a3ec3fff55d677aab13
Sharperrr <simas6666@gmail.com>
2024-03-20 18:16:07 +0200

    Tests added

--- src/main/java/automated_test_4/Main.java
+++ src/main/java/automated_test_4/Main.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -18,7 +21,9 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException
+	
+	@BeforeClass
+	public static void CreateUser()
 	{
 		System.setProperty("webdriver.edge.driver", "C:\\Users\\simas\\msedgedriver.exe");
 		EdgeDriver driver = new EdgeDriver();
@@ -26,6 +31,8 @@ public class Main {
 		
 		driver.manage().window().maximize();
 		CreateUser(driver, wait);
+		
+		
 		
 		for(int t = 1; t < 3; ++t)
 		{
@@ -90,87 +97,97 @@ public class Main {
 		
 	}
 	
+	@Test
 	static void Testing(EdgeDriver driver, WebDriverWait wait, int dataId)
 	{
-		Wait<EdgeDriver> fluentWait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(30))
-                .pollingEvery(Duration.ofMillis(1000))
-                .ignoring(org.openqa.selenium.TimeoutException.class);
-		
-		driver.get("https://demowebshop.tricentis.com/");
-		
-		driver.findElement(By.xpath("//a[text()='Log in']")).click();
-		
-		String filePath = "user.txt";
-		int id = getUserID(filePath);
-		String email = "automatedtestingdummyemail" + --id + "@gmail.com";
-		String password = "123456";
-
-		driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(email);
-		driver.findElement(By.xpath("//input[@id='Password']")).sendKeys(password);
-		
-		driver.findElement(By.xpath("//div[@class='buttons']/input[@value='Log in']")).click();
-		
-		driver.findElement(By.xpath("//div[@class='master-wrapper-main']//a[@href='/digital-downloads']")).click();
-		
-		String dataFilePath = "data" + dataId + ".txt";
-		try (BufferedReader reader = new BufferedReader(new FileReader(dataFilePath))) {
-            String line;
-            int i = 0;
-            while ((line = reader.readLine()) != null) {
-            	++i;
-            	int j = i;
-            	String line1 = line;
-            	fluentWait.until((EdgeDriver edgeDriver) -> {
-            		driver.findElement(By.xpath("//a[text()='"+ line1 +"']/../..//input[@value='Add to cart']")).click();
-            		try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-            		String expectedQty = '(' + String.valueOf(j) + ')';
-            		
-            		if(driver.findElement(By.xpath("//a[@class='ico-cart']/span[@class='cart-qty']")).getText().equals(expectedQty))
-            		{
-            			return true;
-            		}
-            		else
-            		{
-            			return false;
-            		}
-            	});
-            	
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-		
-		driver.findElement(By.xpath("//a[@href='/cart']")).click();
-		driver.findElement(By.xpath("//input[@id='termsofservice']")).click();
-		driver.findElement(By.xpath("//button[@id='checkout']")).click();
-		
-		if(dataId == 1)
+		for(int t = 1; t < 3; ++t)
 		{
-			driver.findElement(By.xpath("//select[@id='BillingNewAddress_CountryId']")).click();
-			driver.findElement(By.xpath("//option[@value='156']")).click();
-			driver.findElement(By.xpath("//input[@id='BillingNewAddress_City']")).sendKeys("Vilnius");
-			driver.findElement(By.xpath("//input[@id='BillingNewAddress_Address1']")).sendKeys("Didlaukio g. 47");
-			driver.findElement(By.xpath("//input[@id='BillingNewAddress_ZipPostalCode']")).sendKeys("LT-08303");
-			driver.findElement(By.xpath("//input[@id='BillingNewAddress_PhoneNumber']")).sendKeys("+37063564966");
+			driver = new EdgeDriver();
+			wait = new WebDriverWait(driver, 5);
+			driver.manage().window().maximize();
+			
+			Wait<EdgeDriver> fluentWait = new FluentWait<>(driver)
+	                .withTimeout(Duration.ofSeconds(30))
+	                .pollingEvery(Duration.ofMillis(1000))
+	                .ignoring(org.openqa.selenium.TimeoutException.class);
+			
+			driver.get("https://demowebshop.tricentis.com/");
+			
+			driver.findElement(By.xpath("//a[text()='Log in']")).click();
+			
+			String filePath = "user.txt";
+			int id = getUserID(filePath);
+			String email = "automatedtestingdummyemail" + --id + "@gmail.com";
+			String password = "123456";
+
+			driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(email);
+			driver.findElement(By.xpath("//input[@id='Password']")).sendKeys(password);
+			
+			driver.findElement(By.xpath("//div[@class='buttons']/input[@value='Log in']")).click();
+			
+			driver.findElement(By.xpath("//div[@class='master-wrapper-main']//a[@href='/digital-downloads']")).click();
+			
+			String dataFilePath = "data" + dataId + ".txt";
+			try (BufferedReader reader = new BufferedReader(new FileReader(dataFilePath))) {
+	            String line;
+	            int i = 0;
+	            while ((line = reader.readLine()) != null) {
+	            	++i;
+	            	int j = i;
+	            	String line1 = line;
+	            	EdgeDriver driver1 = driver;
+	            	fluentWait.until((EdgeDriver edgeDriver) -> {
+	            		driver1.findElement(By.xpath("//a[text()='"+ line1 +"']/../..//input[@value='Add to cart']")).click();
+	            		try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+	            		String expectedQty = '(' + String.valueOf(j) + ')';
+	            		
+	            		if(driver1.findElement(By.xpath("//a[@class='ico-cart']/span[@class='cart-qty']")).getText().equals(expectedQty))
+	            		{
+	            			return true;
+	            		}
+	            		else
+	            		{
+	            			return false;
+	            		}
+	            	});
+	            	
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+			
+			driver.findElement(By.xpath("//a[@href='/cart']")).click();
+			driver.findElement(By.xpath("//input[@id='termsofservice']")).click();
+			driver.findElement(By.xpath("//button[@id='checkout']")).click();
+			
+			if(dataId == 1)
+			{
+				driver.findElement(By.xpath("//select[@id='BillingNewAddress_CountryId']")).click();
+				driver.findElement(By.xpath("//option[@value='156']")).click();
+				driver.findElement(By.xpath("//input[@id='BillingNewAddress_City']")).sendKeys("Vilnius");
+				driver.findElement(By.xpath("//input[@id='BillingNewAddress_Address1']")).sendKeys("Didlaukio g. 47");
+				driver.findElement(By.xpath("//input[@id='BillingNewAddress_ZipPostalCode']")).sendKeys("LT-08303");
+				driver.findElement(By.xpath("//input[@id='BillingNewAddress_PhoneNumber']")).sendKeys("+37063564966");
+			}
+			
+			driver.findElement(By.xpath("//input[@onclick='Billing.save()']")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@onclick='PaymentMethod.save()']")));
+			driver.findElement(By.xpath("//input[@onclick='PaymentMethod.save()']")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@onclick='PaymentInfo.save()']")));
+			driver.findElement(By.xpath("//input[@onclick='PaymentInfo.save()']")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@onclick='ConfirmOrder.save()']")));
+			driver.findElement(By.xpath("//input[@onclick='ConfirmOrder.save()']")).click();
+			
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Click here for order details.']")));
+			driver.findElement(By.xpath("//a[text()='Click here for order details.']")).click();
+			
+			driver.close();
 		}
 		
-		driver.findElement(By.xpath("//input[@onclick='Billing.save()']")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@onclick='PaymentMethod.save()']")));
-		driver.findElement(By.xpath("//input[@onclick='PaymentMethod.save()']")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@onclick='PaymentInfo.save()']")));
-		driver.findElement(By.xpath("//input[@onclick='PaymentInfo.save()']")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@onclick='ConfirmOrder.save()']")));
-		driver.findElement(By.xpath("//input[@onclick='ConfirmOrder.save()']")).click();
-		
-		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='Click here for order details.']")));
-		driver.findElement(By.xpath("//a[text()='Click here for order details.']")).click();
-		
-		driver.close();
 	}
 	
 	static int getUserID(String filePath)
